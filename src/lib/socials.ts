9c247aac5e557e0462a61e3a638356af6bdff43a
ElFietulais <fietu@outlook.es>
2024-04-14 17:54:33 -0500

    🐧

---
+++ src/lib/socials.ts
@@ -0,0 +1,14 @@
+export default [
+    {
+        name: "Github",
+        url: "https://github.com/ElFietulais",
+    },
+    {
+        name: "X (Twitter)",
+        url: "https://twitter.com/ElFietulais",
+    },
+    {
+        name: "Discord",
+        url: "https://discord.com/users/780876656352559125",
+    },
+];
